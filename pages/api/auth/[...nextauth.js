f2166cdb1589ff9d84a482426e0a25433a63e0ba
Ben Gross <benrgross@gmail.com>
2021-06-01 19:43:26 -0400

    pushing

--- pages/api/auth/[...nextauth.js
+++ pages/api/auth/[...nextauth.js
@@ -4,8 +4,10 @@ import Providers from "next-auth/providers";
 const options = {
   providers: [
     Providers.Github({
-      clintId: "",
-      clientSectret: "",
+      clintId: process.env.GITHUB_ID,
+      clientSectret: process.env.GITHUB_SECRET,
     }),
   ],
 };
+
+export default (req, res) => NextAuth(req, res, options);
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/client";
 import Head from "next/head";
-import Image from "next/image";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
