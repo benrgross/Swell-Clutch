5058879c09d5d22812a4d515f4113fb936aea077
Ben Gross <benrgross@gmail.com>
2021-06-01 20:09:19 -0400

    github auth working

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+const options = {
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  database: {
+    type: "mongodb",
+    database: "swell-clutch",
+    synchronize: true,
+  },
+};
+
+export default (req, res) => NextAuth(req, res, options);
