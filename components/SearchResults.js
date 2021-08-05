d1a838f8cb9454cbf5cad315a2ef112f19568e5d
Ben Gross <benrgross@gmail.com>
2021-08-05 18:19:46 -0400

    extractign swell and tide object

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -17,7 +17,8 @@ function SearchResults() {
     const tide = await axios.post(`${server}/api/tides`, id);
 
     // slice data for only today
-    const tides = tide.data.data.tides.slice(0, 17).map(function (tides) {
+    // map unix time to hours
+    const tides = tide.data.data.tides.slice(0, 25).map(function (tides) {
       let date = new Date(tides.timestamp * 1000);
       tides.timestamp = date.getHours();
       return tides;
@@ -28,26 +29,74 @@ function SearchResults() {
       return wave;
     });
 
-    // map unix time to hours
-    // let newSwells = swells.map(function (wave) {
-    //   let date = new Date(wave.timestamp * 1000);
-    //   wave.timestamp = date.getHours();
-    //   return wave;
-
     console.log("swells", swells);
     console.log("tides", tides);
 
-    // let userTime = new Date().getHours();
-    let userTime = 13;
+    // get the users time
+    let userTime = new Date().getHours();
+    // let userTime = 13;
     console.log(userTime);
 
+    let currentSwell;
     for (let i = 0; i < swells.length; i++) {
       if (userTime === swells[i].timestamp) {
-        console.log("test", swells[i]);
+        currentSwell = swells[i];
+      } else if (userTime > 16) {
+        currentSwell = swells[16];
+      } else if (userTime < 3) {
+        currentSwell = swells[0];
       }
     }
+    console.log("currentSwell", currentSwell);
 
-    // match tide via time stamp
+    const currentTide = {
+      height: 0,
+      status: "null",
+    };
+
+    // FIX TIDE BUG TO REFLECT HIGH OR LOW TIDE WHEN TIMESTAMPS MATCH
+    for (let i = 0; i < tides.length; i++) {
+      if (userTime === tides[i].timestamp) {
+        currentTide.height = tides[i].height;
+      } else if (userTime < 3) {
+        currentTide.height = tides[0].height;
+      } else if (userTime > 3) {
+        currentTide.height = tides[23].height;
+      }
+
+      if (
+        tides[i].timestamp != 23 &&
+        tides[i].type == "NORMAL" &&
+        tides[i].height > tides[i + 1]
+      ) {
+        currentTide.status = "droping";
+      }
+      if (
+        tides[i].timestamp != 23 &&
+        tides[i].type == "NORMAL" &&
+        tides[i].height < tides[i + 1]
+      ) {
+        currentTide.status = "rising";
+      }
+      if (
+        tides[i].timestamp === 17 &&
+        tides[i].type == "NORMAL" &&
+        tides[i].height < tides[i - 1].height
+      ) {
+        currentTide.status = "dropping";
+      }
+      if (
+        tides[i].timestamp === 17 &&
+        tides[i].type == "NORMAL" &&
+        tides[i].height > tides[i - 1].height
+      ) {
+        currentTide.status = "rising";
+      } else if (tides[i].type === "HIGH") currentTide.status === "high";
+      else if (tides[i].type === "LOW") currentTide.status === "low";
+    }
+
+    console.log(currentTide);
+
     // match swell via time stamp
     //send object to backend and store in db
   };
